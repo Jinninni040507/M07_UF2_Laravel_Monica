2563f9d56470f08d067a58db66543491f12282a5
Jinninni040507 <moni.oliveras05@gmail.com>
2025-02-09 20:08:47 +0100

    #9 Create Film endpoint implemented

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,9 @@ Route::middleware('year')->group(function () {
         Route::get('countFilms/', [FilmController::class, "countFilms"])->name('countFilms');
     });
 });
+
+Route::middleware('url')->group(function () {
+    Route::group(['prefix' => 'filmin'], function () {
+        Route::post('createFilm/', [FilmController::class, "createFilm"])->name('createFilm');
+    });
+});
