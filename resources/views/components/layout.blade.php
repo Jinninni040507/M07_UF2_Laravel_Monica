d7541b25ca6b7eddf8e4a758ed0036b796268ecf
Jinninni040507 <moni.oliveras05@gmail.com>
2025-03-23 21:03:25 +0100

    #36 feat: api delete actor by id implemented

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,12 +26,9 @@
                       <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/">Home</a>
                       </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="/filmin/createFilmForm">Añadir Película</a>
-                      </li>
                       <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Buscar Pelis
+                          Películas
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href=/filmout/films>Todas las pelis</a></li>
@@ -39,11 +36,13 @@
                             <li><a class="dropdown-item" href=/filmout/oldFilms>Pelis antiguas</a></li>
                             <li><a class="dropdown-item" href=/filmout/sortFilms>Pelis ordenadas por año</a></li>
                             <li><a class="dropdown-item" href=/filmout/countFilms>Contar pelis</a></li>
+                            <hr>
+                            <li><a class="dropdown-item" href="/filmin/createFilmForm">Añadir Película</a></li>
                         </ul>
                       </li>
                       <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Buscar Actores
+                          Actores
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href=/actorout/actors>Todos los actores</a></li>
